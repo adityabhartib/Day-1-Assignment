580c6107a96b3957fc575ad99cf05aa9c1048b0c
adityabhartib <adityabharti.ab@gmail.com>
2022-10-10 08:37:24 +0530

    Perfect Square Root Addedd

---
+++ src/com/bridgelabz/AssignmentDay3/TwoDArray.java
@@ -0,0 +1,30 @@
+package com.bridgelabz.AssignmentDay3;
+
+import java.util.Scanner;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Row: ");
+        int row = sc.nextInt();
+        System.out.println("Enter Collum: ");
+        int collum = sc.nextInt();
+        int arr[][] = new int[row][collum];
+
+        System.out.println("Enter Arrays Element:");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+//                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("------------Matrix--------------");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
--- src/com/bridgelabz/basics/PerfectSquare.java
+++ src/com/bridgelabz/basics/PerfectSquare.java
@@ -6,12 +6,10 @@ public class PerfectSquare {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number: ");
-        int num1 = sc.nextInt();
-        System.out.print("Enter Square Number: ");
-         int num2= sc.nextInt();
-         if (num2 ==num1*num1)
-             System.out.println(num2+" is Perfect Square of >"+num1);
-         else
-             System.out.println(num2+" is Not a Perfect Square of >"+num1);
+        int num = sc.nextInt();
+        double m=Math.sqrt(num);
+        System.out.println("Square root of " +num+" is "+m);
+
+
     }
 }
